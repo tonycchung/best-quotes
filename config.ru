af6ff1a44c178ea6ea67f98eab4bd40b16134f42
Tony Chung <anthonyuchung@gmail.com>
2014-06-14 14:24:09 -0700

    First commit Completed chapter 2

---
+++ config.ru
@@ -0,0 +1,3 @@
+# best_quotes/config.ru
+require './config/application.rb'
+run BestQuotes::Application.new
---
+++ config/application.rb
@@ -0,0 +1,13 @@
+# best_quotes/config/application.rb
+require "rulers"
+
+$LOAD_PATH << File.join(File.dirname(__FILE__),
+                        "..", "app",
+                        "controllers")
+require "quotes_controller"
+
+module BestQuotes
+  class Application < Rulers::Application
+  end
+end
+
