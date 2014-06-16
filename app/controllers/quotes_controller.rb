586c2bcde2334ae36be3ddd14ad6dc91314fd09c
Tony Chung <anthonyuchung@gmail.com>
2014-06-16 16:28:35 -0700

    Add and index new quotes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,9 +1,29 @@
 class QuotesController < Rulers::Controller
+  def index
+    quotes = FileModel.all
+    render :index, :quotes => quotes
+  end
+
   def a_quote
     render :a_quote, :noun => :winking
   end
 
   def exception
     raise "It's a bad one!"
+  end
+
+  def quote_1
+    quote_1 = Rulers::Model::FileModel.find(1)
+    render :quote, :obj => quote_1
+  end
+
+  def new_quote
+    attrs = {
+      "submitter" => "web user",
+      "quote" => "A picture is worth one k pixels",
+      "attribution" => "Me"
+    }
+    m = FileModel.create attrs
+    render :quote, :obj => m
   end
 end
