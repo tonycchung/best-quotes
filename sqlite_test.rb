7cd58c3df3de29fb227338b4bbff4f8b985bb29e
Tony Chung <anthonyuchung@gmail.com>
2014-06-20 17:05:20 -0700

    Change test for saving objects

--- sqlite_test.rb
+++ sqlite_test.rb
@@ -6,11 +6,17 @@ STDERR.puts MyTable.schema.inspect
 
 # Create row
 mt = MyTable.create "title" => "I saw it again!"
+mt["title"] = "I really did!"
+mt.save!
 
-puts "Count: #{MyTable.count}"
+mt2 = MyTable.find mt["id"]
 
-top_id = mt["id"].to_i
-(1..top_id).each do |id|
-  mt_id = MyTable.find(id)
-  puts "Found title #{mt_id["title"]}."
-end
+puts "Title: #{mt2["title"]}"
+
+# puts "Count: #{MyTable.count}"
+
+# top_id = mt["id"].to_i
+# (1..top_id).each do |id|
+#   mt_id = MyTable.find(id)
+#   puts "Found title #{mt_id["title"]}."
+# end
